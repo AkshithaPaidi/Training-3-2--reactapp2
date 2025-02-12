e5b5c0faaecf66224fc845354ede0d94db05002e
Akshitha Reddy Paidi <akshithareddy.paidi@gmail.com>
2025-02-12 12:34:37 +0530

    react-routing

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import "./global.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
+
+
---
+++ src/pages/Assessment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Assessment = () => {
+  return (
+    <div>Assessments</div>
+  )
+}
+
+export default Assessment
